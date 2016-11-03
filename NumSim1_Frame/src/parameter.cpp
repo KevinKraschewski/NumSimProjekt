fd25481673d7b53e3facad93aa866528cd56da57
KevinKraschewski <kevin.kraschewski@web.de>
2016-11-03 13:16:20 +0100

    config-Parser Datei hochgeladen und in die Load Methode von parameter kopiert.

--- NumSim1_Frame/src/parameter.cpp
+++ NumSim1_Frame/src/parameter.cpp
@@ -17,6 +17,9 @@
 
 #include "typedef.hpp"
 #include "parameter.hpp"
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
   /// Constructs a new Parameter set with default values
   // Driven Cavity parameters; see exercise sheet 1
@@ -25,8 +28,59 @@
   }
 
   /// Loads the parameter values from a file
+  /// Nur den Inhalt aus der config_parser Datei rein kopiert...
   void Parameter::Load(const char *file){
-	  // MUSS NOCH WAS GEANDERT WERDEN
+	  FILE* handle = fopen(file,"r");
+	  double inval[2];
+
+	  char name[20];
+	  while (!feof(handle)) {
+	  	if (!fscanf(handle, "%s =", name)) continue;
+	  	if (strcmp(name,"size") == 0) {
+	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
+	  			_size[0] = inval[0];
+	  			_size[1] = inval[1];
+	  		}
+	  		continue;
+	  	}
+	  	if (strcmp(name,"length") == 0) {
+	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
+	  			_length[0] = inval[0];
+	  			_length[1] = inval[1];
+	  		}
+	  		continue;
+	  	}
+	  	if (strcmp(name,"velocity") == 0) {
+	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
+	  			_velocity[0] = inval[0];
+	  			_velocity[1] = inval[1];
+	  		}
+	  		continue;
+	  	}
+	  	if (strcmp(name,"pressure") == 0) {
+	  		if (fscanf(handle," %lf\n",&inval[0]))
+	  			_pressure = inval[0];
+	  		continue;
+	  	}
+	  }
+	  fclose(handle);
+
+	  FILE* handle = fopen(file,"r");
+	  double inval;
+	  char name[20];
+	  while (!feof(handle)) {
+	  	if (!fscanf(handle, "%s = %lf\n", name, &inval)) continue;
+	  	if (strcmp(name,"re") == 0) _re = inval;
+	  	else if (strcmp(name,"omg") == 0) _omega = inval;
+	  	else if (strcmp(name,"alpha") == 0) _alpha = inval;
+	  	else if (strcmp(name,"dt") == 0) _dt = inval;
+	  	else if (strcmp(name,"tend") == 0) _tend = inval;
+	  	else if (strcmp(name,"iter") == 0) _itermax = inval;
+	  	else if (strcmp(name,"eps") == 0) _eps = inval;
+	  	else if (strcmp(name,"tau") == 0) _tau = inval;
+	  	else printf("Unknown parameter %s\n",name);
+	  }
+	  fclose(handle);
   }
 
   /// Getter functions for all parameters
