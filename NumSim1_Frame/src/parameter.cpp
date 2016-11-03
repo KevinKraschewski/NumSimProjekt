ab1512567087008172c87ea71dcb50992228bb7d
KevinKraschewski <kevin.kraschewski@web.de>
2016-11-03 13:48:59 +0100

    Parameter sollte so stimmen und Default Dateien fuer Geometrie und Parameter

--- NumSim1_Frame/src/parameter.cpp
+++ NumSim1_Frame/src/parameter.cpp
@@ -14,56 +14,35 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "typedef.hpp"
+ /// Header
 #include "parameter.hpp"
+
+/// Bibliotheken
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
 
   /// Constructs a new Parameter set with default values
   // Driven Cavity parameters; see exercise sheet 1
-  Parameter(){
-	  // MUSS NOCH WAS GEANDERT WERDEN
+  Parameter::Parameter(){
+	  _re = 1.0;
+	  _omega = 0.0;
+	  _alpha = 0.0;
+	  _dt = 0.5;
+	  _tend = 10;
+	  _eps = 0.1;
+	  _tau = 0;
+	  _itermax = 1000;
+  }
+  
+  // Damit er es wieder aufraeumt..
+  Parameter::~Parameter(){
+	  
   }
 
   /// Loads the parameter values from a file
   /// Nur den Inhalt aus der config_parser Datei rein kopiert...
   void Parameter::Load(const char *file){
-	  FILE* handle = fopen(file,"r");
-	  double inval[2];
-
-	  char name[20];
-	  while (!feof(handle)) {
-	  	if (!fscanf(handle, "%s =", name)) continue;
-	  	if (strcmp(name,"size") == 0) {
-	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
-	  			_size[0] = inval[0];
-	  			_size[1] = inval[1];
-	  		}
-	  		continue;
-	  	}
-	  	if (strcmp(name,"length") == 0) {
-	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
-	  			_length[0] = inval[0];
-	  			_length[1] = inval[1];
-	  		}
-	  		continue;
-	  	}
-	  	if (strcmp(name,"velocity") == 0) {
-	  		if (fscanf(handle," %lf %lf\n",&inval[0],&inval[1])) {
-	  			_velocity[0] = inval[0];
-	  			_velocity[1] = inval[1];
-	  		}
-	  		continue;
-	  	}
-	  	if (strcmp(name,"pressure") == 0) {
-	  		if (fscanf(handle," %lf\n",&inval[0]))
-	  			_pressure = inval[0];
-	  		continue;
-	  	}
-	  }
-	  fclose(handle);
 
 	  FILE* handle = fopen(file,"r");
 	  double inval;
@@ -71,7 +50,7 @@
 	  while (!feof(handle)) {
 	  	if (!fscanf(handle, "%s = %lf\n", name, &inval)) continue;
 	  	if (strcmp(name,"re") == 0) _re = inval;
-	  	else if (strcmp(name,"omg") == 0) _omega = inval;
+	  	else if (strcmp(name,"omg") == 0) _omega = inval; // Bei Gleichheit -> 0 zurueck
 	  	else if (strcmp(name,"alpha") == 0) _alpha = inval;
 	  	else if (strcmp(name,"dt") == 0) _dt = inval;
 	  	else if (strcmp(name,"tend") == 0) _tend = inval;
