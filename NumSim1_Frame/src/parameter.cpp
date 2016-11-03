3d5f2c4de966cb554d57c2918bc5554c1026c32a
KevinKraschewski <kevin.kraschewski@web.de>
2016-11-03 12:59:53 +0100

    Getter Methoden fuer die Parameter Attribute geschrieben

--- NumSim1_Frame/src/parameter.cpp
+++ NumSim1_Frame/src/parameter.cpp
@@ -16,38 +16,58 @@
  */
 
 #include "typedef.hpp"
-//------------------------------------------------------------------------------
-#ifndef __PARAMETER_HPP
-#define __PARAMETER_HPP
-//------------------------------------------------------------------------------
-class Parameter {
-public:
+#include "parameter.hpp"
+
   /// Constructs a new Parameter set with default values
   // Driven Cavity parameters; see exercise sheet 1
-  Parameter();
+  Parameter(){
+	  // MUSS NOCH WAS GEANDERT WERDEN
+  }
 
   /// Loads the parameter values from a file
-  void Load(const char *file);
+  void Parameter::Load(const char *file){
+	  // MUSS NOCH WAS GEANDERT WERDEN
+  }
 
   /// Getter functions for all parameters
-  const real_t &Re() const;
-  const real_t &Omega() const;
-  const real_t &Alpha() const;
-  const real_t &Dt() const;
-  const real_t &Tend() const;
-  const index_t &IterMax() const;
-  const real_t &Eps() const;
-  const real_t &Tau() const;
-
-private:
-  real_t _re;
-  real_t _omega;
-  real_t _alpha;
-  real_t _dt;
-  real_t _tend;
-  real_t _eps;
-  real_t _tau;
-  index_t _itermax;
-};
-//------------------------------------------------------------------------------
-#endif // __PARAMETER_HPP
+  /// Nur ueber return. Bessere Moeglichkeit existiert wohl :)
+  
+  /// Reynoldszahl, fuer Charakteristik der Stroemung
+  const real_t Parameter::&Re() const{
+	  return _Re;
+  }
+  
+  /// Relaxationsfaktor fuer den Loeser
+  const real_t Parameter::&Omega() const{
+	  return _omega;
+  }
+  
+  ///  Alpha aus dem Donor-Cell Scheme
+  const real_t Parameter::&Alpha() const{
+	  return _alpha;
+  }
+  
+  /// Zeitschrittweite
+  const real_t Parameter::&Dt() const{
+	  return _dt;
+  }
+  
+  /// Endzeitpunkt
+  const real_t Parameter::&Tend() const{
+	  return _tend;
+  }
+  
+  /// Zahl der maximalen Iterationen, fuer Abbruchbedingung
+  const index_t Parameter::&IterMax() const{
+	  return _itermax;
+  }
+  
+  /// Epsilon fuer die Abbruchbedingung bei der Iteration fuer den Druck
+  const real_t Parameter::&Eps() const{
+	  return _eps;
+  }
+  
+  /// Tau fuer die Stabilitaetsbedingung in der Zeit ( anstatt \leq RS -> < tau*RS)
+  const real_t Parameter::&Tau() const{
+	  return _tau;
+  }
